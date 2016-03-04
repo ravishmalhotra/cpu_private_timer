02d8029951b79669f850a9b9915f473f871a3ac4
ravishmalhotra <rmalhot@umich.edu>
2016-03-03 18:38:51 -0800

    Demo that uses Zynq CPU private timer for interrupts

---
+++ cpu_private_timer.sdk/cpu_private_timer/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.d: ../src/platform.c \
+ ../../cpu_private_timer_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../cpu_private_timer_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../cpu_private_timer_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../cpu_private_timer_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../cpu_private_timer_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../cpu_private_timer_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../cpu_private_timer_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../cpu_private_timer_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
